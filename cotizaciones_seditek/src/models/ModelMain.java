d3f387c743a85b4965001822781f13b180df9804
JocelynFlores1 <37678163+JocelynFlores1@users.noreply.github.com>
2021-02-05 22:10:43 -0600

    Modificacion ModelMain
    Creación de los métodos de la tabla Usuarios

--- cotizaciones_seditek/src/models/ModelMain.java
+++ cotizaciones_seditek/src/models/ModelMain.java
@@ -10,5 +10,34 @@ package models;
  * @author flore
  */
 public class ModelMain {
+    private String nombre_usuario;
+    private String tipo_usuario;
+    private String password;
+
+
+    public String getUsuario() {
+        return nombre_usuario;
+    }
+
+    public void setUsuario(String nombre_usuario) {
+        this.nombre_usuario = nombre_usuario;
+    }
     
+    
+    public String getTipoUsuario() {
+        return tipo_usuario;
+    }
+
+    public void setTipoUsuario(String tipo_usuario) {
+        this.tipo_usuario = tipo_usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }

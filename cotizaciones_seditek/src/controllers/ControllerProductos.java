72e8eafca0112266cf50509fc87d168f699a44bd
Jorge Luis Aguilar <31743985+jorch17@users.noreply.github.com>
2021-02-11 15:26:36 -0600

    Controladores
    Controladores restantes de proveedores
    y de productos

--- cotizaciones_seditek/src/controllers/ControllerProductos.java
+++ cotizaciones_seditek/src/controllers/ControllerProductos.java
@@ -7,32 +7,32 @@ import views.ViewProductos;
 public class ControllerProductos {
     
     ModelProductos model_productos;
-    ViewProductos views_productos;
+    ViewProductos view_productos;
     
     
     public ControllerProductos(ModelProductos model_productos,ViewProductos views_productos){
         this.model_productos = model_productos;
-        this.views_productos = views_productos;
+        this.view_productos = views_productos;
         
-        views_productos.jBActualizarProductos.addActionListener(event -> jBActualizarProductos_click());
-        views_productos.jBAgregarProductos.addActionListener(event -> jBAgregarProductos_click());
-        views_productos.jBEliminarProductos.addActionListener(event -> jBEliminarProductos_click());
-        views_productos.jBLimpiarProductos.addActionListener(event -> jBLimpiarProductos_click());
+        view_productos.jBActualizarProductos.addActionListener(event -> jBActualizarProductos_click());
+        view_productos.jBAgregarProductos.addActionListener(event -> jBAgregarProductos_click());
+        view_productos.jBEliminarProductos.addActionListener(event -> jBEliminarProductos_click());
+        view_productos.jBLimpiarProductos.addActionListener(event -> jBLimpiarProductos_click());
         
     }
     
-        public void jBLimpiarProductos_click(){
-        views_productos.jTIdProductos.setText(" ");
-        views_productos.jTCodigoProductos.setText(" ");
-        views_productos.jTNombreProductos.setText(" ");
-        views_productos.jTMarcaProductos.setText(" ");
-        views_productos.jTModeloProductos.setText(" ");
-        views_productos.jTDescripcionUsuarioProductos.setText(" ");
-        views_productos.jTDescripcionClienteProductos.setText(" ");
-        views_productos.jTAccesoriosProductos.setText(" ");
-        views_productos.jTPrecioProductos.setText(" ");
-        views_productos.jTStockProductos.setText(" ");
-        views_productos.jTProveedorProductos.setText(" ");
+    public void jBLimpiarProductos_click(){
+        view_productos.jTIdProductos.setText(" ");
+        view_productos.jTCodigoProductos.setText(" ");
+        view_productos.jTNombreProductos.setText(" ");
+        view_productos.jTMarcaProductos.setText(" ");
+        view_productos.jTModeloProductos.setText(" ");
+        view_productos.jTDescripcionUsuarioProductos.setText(" ");
+        view_productos.jTDescripcionClienteProductos.setText(" ");
+        view_productos.jTAccesoriosProductos.setText(" ");
+        view_productos.jTPrecioProductos.setText(" ");
+        view_productos.jTStockProductos.setText(" ");
+        view_productos.jTProveedorProductos.setText(" ");
     }// boton nuevo
     
     public void jBAgregarProductos_click(){
@@ -55,18 +55,18 @@ public class ControllerProductos {
     }//boton borrar
     
     
-        public void getValores(){
-        views_productos.jTIdProductos.setText(""+model_productos.getId_producto());
-        views_productos.jTCodigoProductos.setText(""+model_productos.getCodigo_producto());
-        views_productos.jTNombreProductos.setText(""+model_productos.getNombre_producto());
-        views_productos.jTMarcaProductos.setText(""+model_productos.getMarca());
-        views_productos.jTModeloProductos.setText(""+model_productos.getModelo());
-        views_productos.jTDescripcionUsuarioProductos.setText(""+model_productos.getDescripcion_usuario());
-        views_productos.jTDescripcionClienteProductos.setText(""+model_productos.getDescripcion_cliente());
-        views_productos.jTAccesoriosProductos.setText(""+model_productos.getAccesorios());
-        views_productos.jTPrecioProductos.setText(""+model_productos.getPrecio_unitario());
-        views_productos.jTStockProductos.setText(""+model_productos.getStock());
-        views_productos.jTProveedorProductos.setText(""+model_productos.getId_proveedor());
+    public void getValores(){
+        view_productos.jTIdProductos.setText(""+model_productos.getId_producto());
+        view_productos.jTCodigoProductos.setText(""+model_productos.getCodigo_producto());
+        view_productos.jTNombreProductos.setText(""+model_productos.getNombre_producto());
+        view_productos.jTMarcaProductos.setText(""+model_productos.getMarca());
+        view_productos.jTModeloProductos.setText(""+model_productos.getModelo());
+        view_productos.jTDescripcionUsuarioProductos.setText(""+model_productos.getDescripcion_usuario());
+        view_productos.jTDescripcionClienteProductos.setText(""+model_productos.getDescripcion_cliente());
+        view_productos.jTAccesoriosProductos.setText(""+model_productos.getAccesorios());
+        view_productos.jTPrecioProductos.setText(""+model_productos.getPrecio_unitario());
+        view_productos.jTStockProductos.setText(""+model_productos.getStock());
+        view_productos.jTProveedorProductos.setText(""+model_productos.getId_proveedor());
         
         
         
@@ -76,21 +76,21 @@ public class ControllerProductos {
     public void setValores(){
         
         System.out.print(model_productos);
-        model_productos.setId_producto(views_productos.jTIdProductos.getText());
-        model_productos.setCodigo_producto(views_productos.jTCodigoProductos.getText());
-        model_productos.setNombre_producto(views_productos.jTNombreProductos.getText());
-        model_productos.setMarca(views_productos.jTCodigoProductos.getText());
-        model_productos.setModelo(views_productos.jTNombreProductos.getText());
-        model_productos.setDescripcion_usuario(views_productos.jTCodigoProductos.getText());
-        model_productos.setDescripcion_cliente(views_productos.jTNombreProductos.getText());
-        model_productos.setAccesorios(views_productos.jTCodigoProductos.getText());
-        if(!views_productos.jTPrecioProductos.getText().equals(" ")){
-            model_productos.setPrecio_unitario(Float.parseFloat(views_productos.jTPrecioProductos.getText()));
+        model_productos.setId_producto(view_productos.jTIdProductos.getText());
+        model_productos.setCodigo_producto(view_productos.jTCodigoProductos.getText());
+        model_productos.setNombre_producto(view_productos.jTNombreProductos.getText());
+        model_productos.setMarca(view_productos.jTCodigoProductos.getText());
+        model_productos.setModelo(view_productos.jTNombreProductos.getText());
+        model_productos.setDescripcion_usuario(view_productos.jTCodigoProductos.getText());
+        model_productos.setDescripcion_cliente(view_productos.jTNombreProductos.getText());
+        model_productos.setAccesorios(view_productos.jTCodigoProductos.getText());
+        if(!view_productos.jTPrecioProductos.getText().equals(" ")){
+            model_productos.setPrecio_unitario(Float.parseFloat(view_productos.jTPrecioProductos.getText()));
         } 
-        if(!views_productos.jTStockProductos.getText().equals(" ")){
-            model_productos.setStock(Integer.parseInt(views_productos.jTStockProductos.getText()));
+        if(!view_productos.jTStockProductos.getText().equals(" ")){
+            model_productos.setStock(Integer.parseInt(view_productos.jTStockProductos.getText()));
         }
-        model_productos.setId_proveedor(views_productos.jTProveedorProductos.getText());
+        model_productos.setId_proveedor(view_productos.jTProveedorProductos.getText());
     }
     
     
--- cotizaciones_seditek/src/controllers/ControllerProveedores.java
+++ cotizaciones_seditek/src/controllers/ControllerProveedores.java
@@ -1,14 +1,106 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controllers;
 
-/**
- *
- * @author user
- */
+import models.ModelProveedores;
+import views.ViewProveedores;
+
 public class ControllerProveedores {
+    
+    ModelProveedores model_proveedores;
+    ViewProveedores view_proveedores;
+    
+    public ControllerProveedores(ModelProveedores model_proveedores,ViewProveedores view_proveedores  ){
+        this.model_proveedores = model_proveedores;
+        this. view_proveedores = view_proveedores;
+        
+        view_proveedores.jBActualizarProveedores.addActionListener(event -> jBActualizarProveedores_click());
+        view_proveedores.jBAgregarProveedores.addActionListener(event -> jBAgregarProveedores_click());
+        view_proveedores.jBEliminarProveedores.addActionListener(event -> jBEliminarProveedores_click());
+        view_proveedores.jBLimpiarProveedores.addActionListener(event -> jBLimpiarProveedores_click());
+        
+    }
+    
+       
+       
+       
+    
+    public void jBLimpiarProveedores_click(){
+        view_proveedores.jTIdProveedores.setText(" ");
+        view_proveedores.jTNombreProveedores.setText(" ");
+        view_proveedores.jTTelefonoProveedores.setText(" ");
+        view_proveedores.jTTelefonoCProveedores.setText(" ");
+        view_proveedores.jTEmailProveedores.setText(" ");
+        view_proveedores.jTEmailCProveedores.setText(" ");
+        view_proveedores.jTCalleProveedores.setText(" ");
+        view_proveedores.jTColoniaProveedores.setText(" ");
+        view_proveedores.jTMunicipioProveedores.setText(" ");
+        view_proveedores.jTCPProveedores.setText(" ");
+        view_proveedores.jTEmpresaProveedores.setText(" ");
+        view_proveedores.jTRFC.setText(" ");
+    }// boton nuevo
+    
+    
+        
+    public void jBAgregarProveedores_click(){
+        setValores();
+        model_proveedores.insertarProveedores();
+        getValores();
+        
+    }// boton insertar
+    
+    public void jBActualizarProveedores_click(){
+        setValores();
+        model_proveedores.actualizarProveedores();
+        getValores();
+    }//boton actualizar
+    
+    public void jBEliminarProveedores_click(){
+        setValores();
+        model_proveedores.eliminarProveedores();
+        getValores();
+    }//boton borrar
+    
+    
+    public void getValores(){
+        view_proveedores.jTIdProveedores.setText(""+model_proveedores.getId_proveedor());
+        view_proveedores.jTNombreProveedores.setText(""+model_proveedores.getNombre_proveedor());
+        view_proveedores.jTTelefonoProveedores.setText(""+model_proveedores.getTelefono_proveedor());
+        view_proveedores.jTTelefonoCProveedores.setText(""+model_proveedores.getTelefono_contacto());
+        view_proveedores.jTEmailProveedores.setText(""+model_proveedores.getEmail_proveedor());
+        view_proveedores.jTEmailCProveedores.setText(""+model_proveedores.getEmail_contacto());
+        view_proveedores.jTCalleProveedores.setText(""+model_proveedores.getCalle_proveedor());
+        view_proveedores.jTColoniaProveedores.setText(""+model_proveedores.getColonia_proveedor());
+        view_proveedores.jTMunicipioProveedores.setText(""+model_proveedores.getMunicipio_proveedor());
+        view_proveedores.jTCPProveedores.setText(""+model_proveedores.getCp_proveedor());
+        view_proveedores.jTEmpresaProveedores.setText(""+model_proveedores.getEmpresa_proveedor());
+        view_proveedores.jTRFC.setText(""+model_proveedores.getRfc_proveedor());
+        
+           
+    } 
+    
+       
+     public void setValores(){
+        
+        System.out.print(model_proveedores);
+        model_proveedores.setId_proveedor(view_proveedores.jTIdProveedores.getText());
+        model_proveedores.setNombre_proveedor(view_proveedores.jTNombreProveedores.getText());
+        
+        if(!view_proveedores.jTTelefonoProveedores.getText().equals(" ")){
+            model_proveedores.setTelefono_proveedor(Integer.parseInt(view_proveedores.jTTelefonoProveedores.getText()));
+        }
+        if(!view_proveedores.jTTelefonoCProveedores.getText().equals(" ")){
+            model_proveedores.setTelefono_contacto(Integer.parseInt(view_proveedores.jTTelefonoCProveedores.getText()));
+        }       
+        model_proveedores.setEmail_proveedor(view_proveedores.jTEmailProveedores.getText());
+        model_proveedores.setEmail_contacto(view_proveedores.jTEmailCProveedores.getText());
+        model_proveedores.setCalle_proveedor(view_proveedores.jTCalleProveedores.getText());
+        model_proveedores.setColonia_proveedor(view_proveedores.jTColoniaProveedores.getText());
+        model_proveedores.setMunicipio_proveedor(view_proveedores.jTMunicipioProveedores.getText());
+        model_proveedores.setEmpresa_proveedor(view_proveedores.jTEmpresaProveedores.getText());
+        model_proveedores.setRfc_proveedor(view_proveedores.jTRFC.getText());
+        if(!view_proveedores.jTCPProveedores.getText().equals(" ")){
+        model_proveedores.setCp_proveedor(Integer.parseInt(view_proveedores.jTCPProveedores.getText()));
+        }
+        
+   }
     
 }

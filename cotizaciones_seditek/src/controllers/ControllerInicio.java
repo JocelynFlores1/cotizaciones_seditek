345343a3f1e1b2e42860ec4bc69068925456e609
JocelynFlores1 <37678163+JocelynFlores1@users.noreply.github.com>
2021-02-11 21:26:45 -0600

    Programación de la clase controladora de inicio

--- cotizaciones_seditek/src/controllers/ControllerInicio.java
+++ cotizaciones_seditek/src/controllers/ControllerInicio.java
@@ -5,10 +5,19 @@
  */
 package controllers;
 
+import models.ModelInicio;
+import views.ViewInicio;
+
 /**
  *
  * @author flore
  */
 public class ControllerInicio {
-    
+    models.ModelInicio modelInicio;
+    views.ViewInicio viewInicio;
+
+    public ControllerInicio(ModelInicio modelInicio, ViewInicio viewInicio) {
+        this.modelInicio = modelInicio;
+        this.viewInicio = viewInicio;
+    }
 }

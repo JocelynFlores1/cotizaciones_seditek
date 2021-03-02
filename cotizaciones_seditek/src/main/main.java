20071ccc7eca0575d531415e0e09b369c1d8cbbc
Jorge Luis Aguilar <31743985+jorch17@users.noreply.github.com>
2021-03-02 00:12:28 -0600

    Main
    agregue la vista del proveedores al main pero la comente porque todavia no la corrijo

--- cotizaciones_seditek/src/main/main.java
+++ cotizaciones_seditek/src/main/main.java
@@ -12,14 +12,21 @@ public class main {
     public static void main(String[] args) {
         
         
+       /** 
         models.ModelLogin modelLogin = new models.ModelLogin();
         views.ViewLogin viewLogin = new views.ViewLogin();
         controllers.ControllerLogin controllerLogin = new controllers.ControllerLogin(modelLogin, viewLogin);
-        
+        **/
         
         models.ModelProductos model_productos = new models.ModelProductos();
         views.ViewProductos view_productos = new views.ViewProductos();
         controllers.ControllerProductos controller_productos = new controllers.ControllerProductos(model_productos, view_productos);
+        
+        /**
+        models.ModelProveedores model_proveedores = new models.ModelProveedores();
+        views.ViewProveedores view_proveedores = new views.ViewProveedores();
+        controllers.ControllerProveedores controller_proveedores = new controllers.ControllerProveedores(model_proveedores, view_proveedores);
+        **/
         
     }
     

aa96a117e0e4969014c57566e188bcd754c69cae
Jorge Luis Aguilar <31743985+jorch17@users.noreply.github.com>
2021-03-03 14:08:54 -0600

    MVC Clientes
    ya quedo el MVC de CLientes
    1 ya inserta clientes
    2 ya elimina clientes
    3 ya actualiza clientes
    4 ya limpia los campos de clietes
    pero sigue con ese pequeño error
    ala hora de generar el codigo

---
+++ cotizaciones_seditek/src/controllers/ControllerClientes.java
@@ -0,0 +1,331 @@
+package controllers;
+
+import models.ModelClientes;
+import models.ModelConexion;
+import models.ModelGenerarCodigos;
+import views.ViewClientes;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+
+
+
+public class ControllerClientes {
+    
+    public ModelClientes model_clientes;
+    public ViewClientes view_clientes;
+    ModelConexion clienteConexion = new ModelConexion();
+    
+    
+    ActionListener actionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == view_clientes.jBInsertarCliente) {
+                Insertar_cliente_actionPerformed();
+            } else if (e.getSource() == view_clientes.jBModificarCliente) {
+                Modificar_cliente_actionPerformed();
+            } else if (e.getSource() == view_clientes.jBEliminarCliente) {
+                Borrar_cliente_actionListener();
+            } else if (e.getSource() == view_clientes.jBNuevoCliente) {
+                Nuevo_cliente_actionPerformed();
+            }
+        }
+    };
+    
+    
+    public ControllerClientes(ModelClientes model_clientes,ViewClientes views_clientes){
+        this.model_clientes = model_clientes;
+        this.view_clientes = views_clientes;
+        initComponents();
+        setActionListener();
+    }
+    
+    public void initComponents() {
+        view_clientes.setVisible(true);
+        model_clientes.conectarDB(clienteConexion);
+        CambiarCampos();
+        tablaConsulta();
+        System.out.println("Solo se inicia si se abre clientes");
+    }
+    
+    
+    /**
+     * *
+     * Metodo que cambia los campos de los jTextField en ViewProductos
+     */
+    public void CambiarCampos() {
+        
+        view_clientes.jTIdCliente.setText(""+model_clientes.getId_cliente());
+        view_clientes.jTNombreCliente.setText(""+model_clientes.getNombre_cliente());
+        view_clientes.jTApellidoPaterno.setText(""+model_clientes.getApellido_paterno());
+        view_clientes.jTApellidoMaterno.setText(""+model_clientes.getApellido_materno());
+        view_clientes.jTTelefonoCliente.setText(""+model_clientes.getTelefono_cliente());
+        view_clientes.jTTelefonoEmpresa.setText(""+model_clientes.getTelefono_empresa());
+        view_clientes.jTEmailCliente.setText(""+model_clientes.getEmail_cliente());
+        view_clientes.jTRfcCliente.setText(""+model_clientes.getRfc_cliente());
+        view_clientes.jTCalleCliente.setText(""+model_clientes.getCalle_cliente());
+        view_clientes.jTColoniaCliente.setText(""+model_clientes.getColonia_cliente());
+        view_clientes.jTMunicipioCliente.setText(""+model_clientes.getMunicipio_cliente());
+        view_clientes.jTEmpresaCliente.setText(""+model_clientes.getEmpresa_cliente());
+        view_clientes.jTCpCliente.setText(""+model_clientes.getCp_cliente());
+        
+        
+        
+    }
+    
+    /**
+     * Método para agregar el actionListener a cada boton de la vista
+     */
+    public void setActionListener() {
+        view_clientes.jBNuevoCliente.addActionListener(actionListener);
+        view_clientes.jBInsertarCliente.addActionListener(actionListener);
+        view_clientes.jBModificarCliente.addActionListener(actionListener);
+        view_clientes.jBEliminarCliente.addActionListener(actionListener);
+        view_clientes.jTableClientes.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                insertarCamposTabla();
+            }
+        });
+    }
+    
+    
+    /**
+     * *
+     * Modelo que permite insertar el texto obenido en los jTextField de
+     * ViewProductos e inserta los valores en las variables de ModelProductos
+     */
+    public void Insertar_cliente_actionPerformed() {
+        //JOptionPane.showConfirmDialog permite al usuario elegir si realizar la accion del boton solicitado o simplemente cancelarlo
+        int cancelar = JOptionPane.showConfirmDialog(null, "¿Vas a guardar un nuevo cliente?", "Guardar cliente", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if (cancelar == 0) {
+        
+            model_clientes.setId_cliente(view_clientes.jTIdCliente.getText());
+            model_clientes.setNombre_cliente(view_clientes.jTNombreCliente.getText());
+            model_clientes.setApellido_paterno(view_clientes.jTApellidoPaterno.getText());
+            model_clientes.setApellido_materno(view_clientes.jTApellidoMaterno.getText());
+            if(!view_clientes.jTTelefonoCliente.getText().equals(" ")){
+            model_clientes.setTelefono_cliente(Integer.parseInt(view_clientes.jTTelefonoCliente.getText()));
+            } 
+            if(!view_clientes.jTTelefonoEmpresa.getText().equals(" ")){
+            model_clientes.setTelefono_empresa(Integer.parseInt(view_clientes.jTTelefonoEmpresa.getText()));
+            }
+            model_clientes.setEmail_cliente(view_clientes.jTEmailCliente.getText());
+            model_clientes.setRfc_cliente(view_clientes.jTRfcCliente.getText());
+            model_clientes.setCalle_cliente(view_clientes.jTCalleCliente.getText());
+            model_clientes.setColonia_cliente(view_clientes.jTColoniaCliente.getText());
+            model_clientes.setMunicipio_cliente(view_clientes.jTMunicipioCliente.getText());
+            model_clientes.setEmpresa_cliente(view_clientes.jTEmpresaCliente.getText());
+            if(!view_clientes.jTCpCliente.getText().equals(" ")){
+            model_clientes.setCp_cliente(Integer.parseInt(view_clientes.jTCpCliente.getText()));
+            }
+            
+           
+
+            model_clientes.insertarNuevoCliente(clienteConexion);
+            tablaConsulta();
+            JOptionPane.showMessageDialog(null, "Registro almacenado correctamente");
+        } else {
+            ///Respuesta que se obtiene cuando se cancela la accion del boton elegido
+            JOptionPane.showMessageDialog(null, "No se guardo ningun producto");
+        }
+    }
+    
+    
+    
+    public void Modificar_cliente_actionPerformed() {
+        //JOptionPane.showConfirmDialog permite al usuario elegir si realizar la accion del boton solicitado o simplemente cancelarlo
+        int cancelar = JOptionPane.showConfirmDialog(null, "¿Desea actualizar los datos del cliente?", "Guardar cambios", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if (cancelar == 0) {
+            
+            //model_clientes.setId_cliente(view_clientes.jTIdCliente.getText());
+            model_clientes.setNombre_cliente(view_clientes.jTNombreCliente.getText());
+            model_clientes.setApellido_paterno(view_clientes.jTApellidoPaterno.getText());
+            model_clientes.setApellido_materno(view_clientes.jTApellidoMaterno.getText());
+            if(!view_clientes.jTTelefonoCliente.getText().equals(" ")){
+            model_clientes.setTelefono_cliente(Integer.parseInt(view_clientes.jTTelefonoCliente.getText()));
+            } 
+            if(!view_clientes.jTTelefonoEmpresa.getText().equals(" ")){
+            model_clientes.setTelefono_empresa(Integer.parseInt(view_clientes.jTTelefonoEmpresa.getText()));
+            }
+            model_clientes.setEmail_cliente(view_clientes.jTEmailCliente.getText());
+            model_clientes.setRfc_cliente(view_clientes.jTRfcCliente.getText());
+            model_clientes.setCalle_cliente(view_clientes.jTCalleCliente.getText());
+            model_clientes.setColonia_cliente(view_clientes.jTColoniaCliente.getText());
+            model_clientes.setMunicipio_cliente(view_clientes.jTMunicipioCliente.getText());
+            model_clientes.setEmpresa_cliente(view_clientes.jTEmpresaCliente.getText());
+            if(!view_clientes.jTCpCliente.getText().equals(" ")){
+            model_clientes.setCp_cliente(Integer.parseInt(view_clientes.jTCpCliente.getText()));
+            }
+                        
+            
+            model_clientes.modificarDatosCliente(clienteConexion);
+            //Este comando realiza la accion de utlilzar el metodo de modificarDatosUsuario usando el objeto construido en de modelClientes
+            JOptionPane.showMessageDialog(null, "Datos del cliente modificados correctamente");
+            tablaConsulta();
+            //Se usa el metodo tablaConsulta para actualizar los registros en jTableProducto
+
+        } else {
+            ///Respuesta que se obtiene cuando se cancela la accion del boton elegido
+            JOptionPane.showMessageDialog(null, "No se guardo ningun cambio");
+        }
+    }
+    
+    
+    public void Borrar_cliente_actionListener() {
+        //JOptionPane.showConfirmDialog permite al usuario elegir si realizar la accion del boton solicitado o simplemente cancelarlo
+        int cancelar = JOptionPane.showConfirmDialog(null, "¿Desea borrar los datos del cliente?", "Borrar cliente", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if (cancelar == 0) {
+            model_clientes.setId_cliente(view_clientes.jTIdCliente.getText());
+            model_clientes.borrarDatosCliente(clienteConexion);
+            tablaConsulta();
+            JOptionPane.showMessageDialog(null, "Los datos del producto han sido eliminados");
+        } else {
+            ///Respuesta que se obtiene cuando se cancela la accion del boton elegido
+            JOptionPane.showMessageDialog(null, "No se borro ningun registro");
+        }
+    }
+    
+    
+    public void tablaConsulta() {
+        try {
+            DefaultTableModel modelo = new DefaultTableModel();
+            view_clientes.jTableClientes.setModel(modelo);
+            view_clientes.jTableClientes.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+            model_clientes.consultajTableCliente(clienteConexion);
+            
+            ResultSetMetaData rsMd = model_clientes.getRs().getMetaData();
+            int cantidadColumnas = rsMd.getColumnCount();
+
+            modelo.addColumn("ID");
+            modelo.addColumn("Codigo");
+            modelo.addColumn("Nombre");
+            modelo.addColumn("Apellido Paterno");
+            modelo.addColumn("Apellido Materno");
+            modelo.addColumn("Teléfono Cliente");
+            modelo.addColumn("Teléfono Empresa");
+            modelo.addColumn("Email");
+            modelo.addColumn("RFC");
+            modelo.addColumn("Calle");
+            modelo.addColumn("Colonia");
+            modelo.addColumn("Municipio");
+            modelo.addColumn("Empresa");
+            modelo.addColumn("CP");
+            
+            
+     
+            while (model_clientes.getRs().next()) {
+
+                Object[] filas = new Object[cantidadColumnas];
+
+                for (int i = 0; i < cantidadColumnas; i++) {
+
+                    filas[i] = model_clientes.getRs().getObject(i + 1);
+                }
+
+                modelo.addRow(filas);
+
+            }
+            
+        } catch (SQLException ex) {
+            System.out.println("Error" + ex);
+        }
+    }
+    
+    
+    
+    public void insertarCamposTabla() {
+        try {
+            if (view_clientes.jTableClientes.getSelectedRow() != -1) {
+                int fila = view_clientes.jTableClientes.getSelectedRow();
+
+                view_clientes.jTIdCliente.setText(view_clientes.jTableClientes.getValueAt(fila, 0).toString());
+                view_clientes.jTNombreCliente.setText(view_clientes.jTableClientes.getValueAt(fila, 1).toString());
+                view_clientes.jTApellidoPaterno.setText(view_clientes.jTableClientes.getValueAt(fila, 2).toString());
+                view_clientes.jTApellidoMaterno.setText(view_clientes.jTableClientes.getValueAt(fila, 3).toString());
+                view_clientes.jTTelefonoCliente.setText(view_clientes.jTableClientes.getValueAt(fila, 4).toString());
+                view_clientes.jTTelefonoEmpresa.setText(view_clientes.jTableClientes.getValueAt(fila, 5).toString());
+                view_clientes.jTEmailCliente.setText(view_clientes.jTableClientes.getValueAt(fila, 6).toString());
+                view_clientes.jTRfcCliente.setText(view_clientes.jTableClientes.getValueAt(fila, 7).toString());
+                view_clientes.jTCalleCliente.setText(view_clientes.jTableClientes.getValueAt(fila, 8).toString());
+                view_clientes.jTColoniaCliente.setText(view_clientes.jTableClientes.getValueAt(fila, 9).toString());
+                view_clientes.jTMunicipioCliente.setText(view_clientes.jTableClientes.getValueAt(fila, 10).toString());
+                view_clientes.jTEmpresaCliente.setText(view_clientes.jTableClientes.getValueAt(fila, 11).toString());
+                view_clientes.jTCpCliente.setText(view_clientes.jTableClientes.getValueAt(fila, 12).toString());
+                
+                
+               
+                
+            }
+        } catch (Exception err) {
+            JOptionPane.showMessageDialog(null, "Error:\nSelecciona un registro");
+        }
+    }
+    
+    
+    public void Nuevo_cliente_actionPerformed() {
+        
+               
+        //view_clientes.jTIdCliente.setText("");
+        view_clientes.jTNombreCliente.setText("");
+        view_clientes.jTApellidoPaterno.setText("");
+        view_clientes.jTApellidoMaterno.setText("");
+        view_clientes.jTTelefonoCliente.setText("");
+        view_clientes.jTTelefonoEmpresa.setText("");
+        view_clientes.jTEmailCliente.setText("");
+        view_clientes.jTRfcCliente.setText("");
+        view_clientes.jTCalleCliente.setText("");
+        view_clientes.jTColoniaCliente.setText("");
+        view_clientes.jTMunicipioCliente.setText("");
+        view_clientes.jTEmpresaCliente.setText("");
+        view_clientes.jTCpCliente.setText("");
+        codigos();
+        
+    }
+    
+    
+    
+    
+    
+    /**
+     * Este metodo permite generar codigos automaticamente con una consulta en
+     * la base de datos desde models.ModelProductos.GenerarCodigos
+     */
+    public void codigos() {
+
+        int j;
+        int cont = 1;
+        String num = "";
+        model_clientes.consultaGenerarCodigos(clienteConexion);
+
+        if (model_clientes.getU() == null) {
+            view_clientes.jTIdCliente.setText("SEDITEK-C0000001");
+        } else {
+            char r1 = model_clientes.getU().charAt(6);
+            char r2 = model_clientes.getU().charAt(7);
+            char r3 = model_clientes.getU().charAt(8);
+            char r4 = model_clientes.getU().charAt(9);
+            char r5 = model_clientes.getU().charAt(10);
+            char r6 = model_clientes.getU().charAt(11);
+            char r7 = model_clientes.getU().charAt(12);
+
+            String r = "";
+            r = "" + r1 + r2 + r3 + r4 + r5 + r6 + r7;
+            j = Integer.parseInt(r);
+            System.out.println(j);
+            ModelGenerarCodigos gen = new ModelGenerarCodigos();
+            gen.generar(j);
+            view_clientes.jTIdCliente.setText("SEDITEK-C" + gen.serie());
+            System.out.println("SEDITEK-C" + gen.serie());
+
+        }
+    }
+    
+    
+}
---
+++ cotizaciones_seditek/src/models/ModelClientes.java
@@ -0,0 +1,384 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ModelClientes {
+
+    
+    
+    private Connection conexion;
+    private Statement st;
+    private ResultSet rs;
+    private PreparedStatement ps;
+    
+    
+    private String id_cliente;
+    private String nombre_cliente;
+    private String apellido_paterno;
+    private String apellido_materno;
+    private int telefono_cliente;
+    private int telefono_empresa;
+    private String email_cliente;
+    private String rfc_cliente;
+    private String calle_cliente;
+    private String colonia_cliente;
+    private String municipio_cliente;
+    private String empresa_cliente;
+    private int cp_cliente;
+    private String u = "";
+    
+    
+    
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(Connection conexion) {
+        this.conexion = conexion;
+    }
+
+    public Statement getSt() {
+        return st;
+    }
+
+    public void setSt(Statement st) {
+        this.st = st;
+    }
+
+    public ResultSet getRs() {
+        return rs;
+    }
+
+    public void setRs(ResultSet rs) {
+        this.rs = rs;
+    }
+
+    public PreparedStatement getPs() {
+        return ps;
+    }
+
+    public void setPs(PreparedStatement ps) {
+        this.ps = ps;
+    }
+    
+    
+  
+    public String getId_cliente() {
+        return id_cliente;
+    }
+
+
+    public void setId_cliente(String id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+
+    public String getNombre_cliente() {
+        return nombre_cliente;
+    }
+
+
+    public void setNombre_cliente(String nombre_cliente) {
+        this.nombre_cliente = nombre_cliente;
+    }
+
+
+    public String getApellido_paterno() {
+        return apellido_paterno;
+    }
+
+
+    public void setApellido_paterno(String apellido_paterno) {
+        this.apellido_paterno = apellido_paterno;
+    }
+
+
+    public String getApellido_materno() {
+        return apellido_materno;
+    }
+
+
+    public void setApellido_materno(String apellido_materno) {
+        this.apellido_materno = apellido_materno;
+    }
+
+
+    public int getTelefono_cliente() {
+        return telefono_cliente;
+    }
+
+
+    public void setTelefono_cliente(int telefono_cliente) {
+        this.telefono_cliente = telefono_cliente;
+    }
+
+
+    public int getTelefono_empresa() {
+        return telefono_empresa;
+    }
+
+
+    public void setTelefono_empresa(int telefono_empresa) {
+        this.telefono_empresa = telefono_empresa;
+    }
+
+
+    public String getEmail_cliente() {
+        return email_cliente;
+    }
+
+
+    public void setEmail_cliente(String email_cliente) {
+        this.email_cliente = email_cliente;
+    }
+
+
+    public String getRfc_cliente() {
+        return rfc_cliente;
+    }
+
+
+    public void setRfc_cliente(String rfc_cliente) {
+        this.rfc_cliente = rfc_cliente;
+    }
+
+    public String getCalle_cliente() {
+        return calle_cliente;
+    }
+
+
+    public void setCalle_cliente(String calle_cliente) {
+        this.calle_cliente = calle_cliente;
+    }
+
+
+    public String getColonia_cliente() {
+        return colonia_cliente;
+    }
+
+
+    public void setColonia_cliente(String colonia_cliente) {
+        this.colonia_cliente = colonia_cliente;
+    }
+
+
+    public String getMunicipio_cliente() {
+        return municipio_cliente;
+    }
+
+
+    public void setMunicipio_cliente(String municipio_cliente) {
+        this.municipio_cliente = municipio_cliente;
+    }
+
+    
+    public String getEmpresa_cliente() {
+        return empresa_cliente;
+    }
+
+    
+    public void setEmpresa_cliente(String empresa_cliente) {
+        this.empresa_cliente = empresa_cliente;
+    }
+
+    
+    public int getCp_cliente() {
+        return cp_cliente;
+    }
+
+   
+    public void setCp_cliente(int cp_cliente) {
+        this.cp_cliente = cp_cliente;
+    }
+    
+    
+    
+    
+    public void modificarDatos() {
+        try {
+            System.out.println("modificar datos variables 1 clientes");
+            setId_cliente(rs.getString(1));
+            setNombre_cliente(rs.getString(2));
+            setApellido_paterno(rs.getString(3));
+            setApellido_materno(rs.getString(4));
+            setTelefono_cliente(rs.getInt(5));
+            setTelefono_empresa(rs.getInt(6));
+            setEmail_cliente(rs.getString(7));
+            setRfc_cliente(rs.getString(8));
+            setCalle_cliente(rs.getString(9));
+            setColonia_cliente(rs.getString(10));
+            setMunicipio_cliente(rs.getString(11));
+            setEmpresa_cliente(rs.getString(12));
+            setCp_cliente(rs.getInt(13));
+            
+            
+        } catch (SQLException e) {
+            System.out.println("Error 01: modificar datos" + e);
+        }
+    }
+    
+    
+    /**
+     * Método que realiza las siguietnes acciones: 
+     * 1.- Conecta con la base 
+     * 2.- Consulta todo los registros .
+     **/
+    public void conectarDB(ModelConexion clienteConexion) {
+        try {
+            System.out.println("Conexion exitosa");
+            String consultaString = "select * from Clientes";
+            ps = (PreparedStatement) clienteConexion.getConexion().prepareStatement(consultaString);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                modificarDatos();
+            } else {
+                System.out.println("Error de consulta");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error 02: tabla Clientes" + e);
+        }
+    }
+    
+    
+    public void insertarNuevoCliente(ModelConexion clienteConexion) {
+        System.out.println("nuevo  3 Clientes");
+        String sqlInsertarCliente = "INSERT into Clientes(id_cliente,nombre_cliente,apellido_paterno,apellido_materno,"
+                + "telefono_cliente,telefono_empresa,email_cliente,rfc_cliente,calle_cliente,colonia_cliente,municipio_cliente,"
+                + "empresa_cliente,cp_cliente) "
+                    + "values(?,?,?,?,?,?,?,?,?,?,?,?,?);";
+        try {
+            ps = (PreparedStatement) clienteConexion.getConexion().prepareStatement(sqlInsertarCliente); //con este comando se podra hacer la modificacion a la tabla en la base de datos
+            System.out.println(getId_cliente());
+            
+            ps.setString(1, getId_cliente());
+            ps.setString(2, getNombre_cliente());
+            ps.setString(3, getApellido_paterno());
+            ps.setString(4, getApellido_materno());
+            ps.setInt(5, getTelefono_cliente());
+            ps.setInt(6, getTelefono_empresa());
+            ps.setString(7, getEmail_cliente());
+            ps.setString(8, getRfc_cliente());
+            ps.setString(9, getCalle_cliente());
+            ps.setString(10, getColonia_cliente());
+            ps.setString(11, getMunicipio_cliente());
+            ps.setString(12,getEmpresa_cliente());
+            ps.setInt(13,getCp_cliente());
+            
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("Error 03: Insertar nuevo cliente" + ex);
+        }
+    }
+    
+    
+    
+    public void modificarDatosCliente(ModelConexion clienteConexion) {
+        System.out.println("modificar 4 cliente");
+        String sqlModificarCliente = "UPDATE Clientes SET nombre_cliente=?, apellido_paterno=?, apellido_materno=?, telefono_cliente=?, telefono_empresa=?, "
+                    + "email_cliente=?, rfc_cliente=?, calle_cliente=?, colonia_cliente=?, municipio_cliente=?,"
+                + "empresa_cliente=?, cp_cliente=? WHERE id_cliente=?;";
+        try {
+
+            ps = (PreparedStatement) clienteConexion.getConexion().prepareStatement(sqlModificarCliente);
+
+            System.out.println(getId_cliente());
+            
+            
+            ps.setString(1, getNombre_cliente());
+            ps.setString(2, getApellido_paterno());
+            ps.setString(3, getApellido_materno());
+            ps.setInt(4, getTelefono_cliente());
+            ps.setInt(5, getTelefono_empresa());
+            ps.setString(6, getEmail_cliente());
+            ps.setString(7, getRfc_cliente());
+            ps.setString(8, getCalle_cliente());
+            ps.setString(9, getColonia_cliente());
+            ps.setString(10, getMunicipio_cliente());
+            ps.setString(11,getEmpresa_cliente());
+            ps.setInt(12,getCp_cliente());
+            ps.setString(13, getId_cliente());
+            
+            ps.executeUpdate();
+
+        } catch (SQLException ex) {
+            System.out.println("Error 04: Modificar datos cliente" + ex);
+        }
+    }
+    
+    
+    public void borrarDatosCliente(ModelConexion clienteConexion) {
+        System.out.println("eliminar 5 cliente");
+        String sqlBorrarCliente = "DELETE FROM Clientes WHERE id_cliente= ?;";
+        try {
+
+            ps = (PreparedStatement) clienteConexion.getConexion().prepareStatement(sqlBorrarCliente);
+            //Este proceso permite establecer la conexion del objeto creado y enlazar la consulta con la base de datos para poder borrar el producto.
+            ps.setString(1, getId_cliente());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("Error 05: Borrar datos cliente" + ex);
+        }
+    }
+    
+    
+    /**
+     * *
+     * Metodo que permite insertar los datos de la tabla de la base de datos en
+     * un jTable en java
+     */
+    public void consultajTableCliente(ModelConexion clienteConexion) {
+        try {
+            System.out.println("tabla 6 cliente");
+            String consultaString = "select * from Clientes";
+            ps = (PreparedStatement) clienteConexion.getConexion().prepareStatement(consultaString);
+            rs = ps.executeQuery();
+        } catch (SQLException e) {
+            System.out.println("Error 000000: tabla cliente" + e);
+        }
+    }
+    
+    
+    
+        public void consultaGenerarCodigos(ModelConexion productoConexion) {
+        System.out.println("codigos 7 cliente");
+        String SQL = "select max(id_cliente) from Clientes";
+
+        try {
+            ps = (PreparedStatement) productoConexion.getConexion().prepareStatement(SQL);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                u = rs.getString(1);
+                System.out.println("madx" + rs.getString(1));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error" + ex);
+        }
+
+    }
+
+    public String getU() {
+        return u;
+    }
+
+    public void setU(String c) {
+        this.u= u;
+    }
+    
+    
+    
+    
+    
+    
+    
+}

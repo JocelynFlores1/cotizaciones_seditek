e13d78644fa5a274bdd4e874eb4b597d921e0e6d
JocelynFlores1 <37678163+JocelynFlores1@users.noreply.github.com>
2021-03-16 09:53:32 -0600

    NO MODIFICAR

--- cotizaciones_seditek/src/main/main.java
+++ cotizaciones_seditek/src/main/main.java
@@ -10,43 +10,15 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
-       /**
-        //* Vista del Login
+
         models.ModelLogin modelLogin = new models.ModelLogin();
         views.ViewLogin viewLogin = new views.ViewLogin();
         controllers.ControllerLogin controllerLogin = new controllers.ControllerLogin(modelLogin, viewLogin);
-        **/
-       
-       
-       models.ModelClientes model_clientes = new models.ModelClientes();
-       views.ViewClientes view_clientes = new views.ViewClientes();
-       controllers.ControllerClientes controller_clientes = new controllers.ControllerClientes(model_clientes, view_clientes);
-       
-        /**
-        // * Vista de Productos
-        models.ModelProductos model_productos = new models.ModelProductos();
-        views.ViewProductos view_productos = new views.ViewProductos();
-        controllers.ControllerProductos controller_productos = new controllers.ControllerProductos(model_productos, view_productos);
-        **/
         
+        /**models.ModelUsuarios modelUsuarios = new models.ModelUsuarios();
+        views.ViewUsuarios viewUsuarios = new views.ViewUsuarios();
+        controllers.ControllerUsuarios controllerUsuarios = new controllers.ControllerUsuarios(modelUsuarios, viewUsuarios);**/
         
-        /**
-         // Vista del Proveedor
-        models.ModelProveedores model_proveedores = new models.ModelProveedores();
-        views.ViewProveedores view_proveedores = new views.ViewProveedores();
-        controllers.ControllerProveedores controller_proveedores = new controllers.ControllerProveedores(model_proveedores, view_proveedores);
-        **/
-         
-        
-        
-        
-         //* Vista de Servicios
-         
-        models.ModelServicios model_servicios = new models.ModelServicios();
-        views.ViewServicios view_servicios = new views.ViewServicios();
-        controllers.ControllerServicios controller_servicios = new controllers.ControllerServicios(model_servicios, view_servicios);
         
     }
     

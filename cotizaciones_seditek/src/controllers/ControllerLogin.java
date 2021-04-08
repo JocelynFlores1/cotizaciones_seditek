fc8a51de44c07d307534f3fe89c26c31e683c394
JocelynFlores1 <37678163+JocelynFlores1@users.noreply.github.com>
2021-04-08 11:34:41 -0500

    Modificaciones realizadas

--- cotizaciones_seditek/src/controllers/ControllerLogin.java
+++ cotizaciones_seditek/src/controllers/ControllerLogin.java
@@ -37,7 +37,7 @@ public class ControllerLogin implements ActionListener {
         viewLogin.setLocationRelativeTo(null);
         viewLogin.setTitle("Inicio de sesión Seditek");
 
-        ImageIcon imagenUno = new ImageIcon(getClass().getResource("/img/login.jpg"));
+        ImageIcon imagenUno = new ImageIcon(getClass().getResource("/img/LOGO-SEDITEK.jpg"));
         Image fondoUno = imagenUno.getImage().getScaledInstance(viewLogin.jl_Login.getWidth(), viewLogin.jl_Login.getHeight(), Image.SCALE_SMOOTH);
         Icon iconoEscalado = new ImageIcon(fondoUno);
         viewLogin.jl_Login.setIcon(iconoEscalado);

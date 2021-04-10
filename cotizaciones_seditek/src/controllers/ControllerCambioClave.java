04d2a31e5308a5a68f5f5471ee6ef8067f7d6438
JocelynFlores1 <37678163+JocelynFlores1@users.noreply.github.com>
2021-04-09 23:48:18 -0500

    Controlador para el cambio de contraseña

---
+++ cotizaciones_seditek/src/controllers/ControllerCambioClave.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import models.ModelCambioClave;
+import views.ViewCambioClave;
+
+/**
+ *
+ * @author flore
+ */
+public class ControllerCambioClave {
+
+    ControllerCambioClave(ModelCambioClave modelCambioClave, ViewCambioClave viewCambioClave, String nombre_usuario, String pregunta, String respuesta) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

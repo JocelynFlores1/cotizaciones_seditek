c0c1a9e70823c30b1c98b189e568d4a7a82dbdfe
JocelynFlores1 <37678163+JocelynFlores1@users.noreply.github.com>
2021-04-10 21:24:15 -0500

    Controlador funcionando

--- cotizaciones_seditek/src/controllers/ControllerCambioClave.java
+++ cotizaciones_seditek/src/controllers/ControllerCambioClave.java
@@ -5,7 +5,13 @@
  */
 package controllers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import models.ModelCambioClave;
+import models.ModelConexion;
 import views.ViewCambioClave;
 
 /**
@@ -13,9 +19,57 @@ import views.ViewCambioClave;
  * @author flore
  */
 public class ControllerCambioClave {
-
+        public models.ModelCambioClave modelCambioClave;
+        public views.ViewCambioClave viewCambioClave;
+        ModelConexion CambioClaveConexion = new ModelConexion();
+        
+        
+        
+        ActionListener actionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == viewCambioClave.jb_aceptar) {
+                jmi_modificarC_actionPerformed();
+            } 
+        }
+    };
     ControllerCambioClave(ModelCambioClave modelCambioClave, ViewCambioClave viewCambioClave, String nombre_usuario, String pregunta, String respuesta) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.modelCambioClave = modelCambioClave;
+        this.viewCambioClave = viewCambioClave;
+        initComponents();
+        setActionListener();
+    }
+    
+    
+    public void initComponents() {
+        viewCambioClave.setVisible(true);
+        modelCambioClave.conectarDB(CambioClaveConexion);
+        System.out.println("Solo se inicia si se abre cambiar clave");
+    }
+    
+    
+    /**
+     * Método para agregar el actionListener a cada boton de la vista
+     */
+    public void setActionListener() {
+        viewCambioClave.jb_aceptar.addActionListener(actionListener);
+    }
+    
+    public void jmi_modificarC_actionPerformed() {
+
+        //JOptionPane.showConfirmDialog permite al usuario elegir si realizar la accion del boton solicitado o simplemente cancelarlo
+        int cancelar = JOptionPane.showConfirmDialog(null, "¿Desea actualizar los datos del usuario?", "Guardar cambios", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if (cancelar == 0) {
+            modelCambioClave.setPassword(viewCambioClave.jt_confirmar.getText());
+
+            modelCambioClave.modificarDatosClave(CambioClaveConexion);
+            JOptionPane.showMessageDialog(null, "Datos del usuario modificados correctamente");
+            //Se usa el metodo tablaConsulta para actualizar los registros en jTableProducto
+
+        } else {
+            ///Respuesta que se obtiene cuando se cancela la accion del boton elegido
+            JOptionPane.showMessageDialog(null, "No se guardo ningun cambio");
+        }
     }
     
 }
